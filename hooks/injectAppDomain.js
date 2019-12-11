58fde95a930f4f99b2dbf717ebb41728e334dbb5
André Grillo <andregrillo@gmail.com>
2019-12-11 17:14:46 +0000

    Fixed Plugin Hook
    Fixed Hook for OS compatibility

--- hooks/injectAppDomain.js
+++ hooks/injectAppDomain.js
@@ -18,15 +18,7 @@ module.exports = function(context) {
             var APP_DOMAIN = getPreferenceValue(config, "APP_DOMAIN")
         }
 
-        //Gets the app name
-        if(process.argv.join("|").indexOf("APP_NAME=") > -1) {
-            var APP_NAME = process.argv.join("|").match(/APP_NAME=(.*?)(\||$)/)[1]
-        } else {
-            var config = fs.readFileSync("config.xml").toString()
-            var APP_NAME = getPreferenceValue(config, "APP_NAME")
-        }
-
-        const platformRoot = path.join(context.opts.projectRoot, 'platforms/ios/'+APP_NAME+'/Resources/app.entitlements');
+        const platformRoot = path.join(context.opts.projectRoot, 'platforms/ios/HelloCordova/Resources/app.entitlements');
 
         var fullXMLPath = path.join("Resources", "app.entitlements");
 
@@ -38,14 +30,13 @@ module.exports = function(context) {
             } else {
 
                 var parser = new DOMParser();
-                /*var xmlContents = fs.readFileSync(result, 'utf-8');*/
                 var doc = parser.parseFromString(result, "text/xml");
 
                 doc.getElementsByTagName("string")[0].textContent = "webcredentials:" + APP_DOMAIN;
                 var xmlSerializer = new XMLSerializer();
                 fs.writeFileSync(platformRoot, xmlSerializer.serializeToString(doc));
 
-                const entitlementsFile = path.join(context.opts.projectRoot, 'platforms/ios/'+APP_NAME+'/Resources/'+APP_NAME+'.entitlements');
+                const entitlementsFile = path.join(context.opts.projectRoot, 'platforms/ios/HelloCordova/Resources/HelloCordova.entitlements');
                 fs.rename(platformRoot, entitlementsFile, function(err) {
                 if ( err ) console.log('ERROR: ' + err);
 });
